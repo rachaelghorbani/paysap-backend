4d385cd9e9f67d238ecfe6da3955333d7b9f567a
rachaelghorbani <rachael.ghorbani@gmail.com>
2020-10-26 18:19:20 -0400

    updated users table

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,7 +2,6 @@ class AuthController < ApplicationController
     skip_before_action :authorized, only: [:create]
   def create
     user = User.find_by(username: user_login_params[:username])
-    byebug
     if user && user.authenticate(user_login_params[:password])
       token = encode_token({ user_id: user.id })
       render json: { user: user, jwt: token }, status: :accepted
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,6 @@ class UsersController < ApplicationController
 
     def create 
         user = User.create(user_params)
-        byebug
         if user.valid?
             token = encode_token(user_id: user.id)
             render json: { user: user, jwt: token }, status: :created
